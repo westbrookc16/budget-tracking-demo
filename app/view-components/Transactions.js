ec7bb12dc6c3f211ae758291599650314a40c2a4
Chris Westbrook <westbchris@gmail.com>
2019-10-20 21:14:01 -0400

    Added the ability to delete transactions.

--- app/view-components/Transactions.js
+++ app/view-components/Transactions.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "@progress/kendo-react-buttons";
 import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import "firebase/firestore";
 import { useFirebaseApp } from "reactfire";
 import { useParams } from "react-router-dom";
 import AddTransaction from "./AddTransaction";
+import { formatMoney } from "../utils/numbers";
 
 const Transactions = () => {
   const { categoryID, budgetID } = useParams();
@@ -21,14 +23,32 @@ const Transactions = () => {
     }
     getCategory();
   }, [firebase, categoryID]);
+  const deleteTransaction = async cat => {
+    try {
+      if (confirm(`Are you sure you want to delete ${cat.name}?`))
+        await firebase
+          .firestore()
+          .collection("transactions")
+          .doc(cat.id)
+          .delete();
+    } catch (e) {
+      console.table(e);
+    }
+  };
   const firebase = useFirebaseApp();
   const [transactions, setTransactions] = useState([]);
+  const [totalSpent, setTotalSpent] = useState(0);
   useEffect(() => {
     const transCollection = firebase
       .firestore()
       .collection("transactions")
       .where("category.id", "==", categoryID);
     return transCollection.onSnapshot(snapShot => {
+      //calculate totals
+      let total = snapShot.docs.reduce((total, doc) => {
+        return total + parseFloat(doc.data().amount);
+      }, 0);
+      setTotalSpent(total);
       setTransactions(
         snapShot.docs.map(doc => {
           return { id: doc.id, ...doc.data(), date: doc.data().date.toDate() };
@@ -43,8 +63,32 @@ const Transactions = () => {
         <Column field="name" title="name" />
         <Column field="date" title="date" format="{0:d}" />
         <Column title="Amount" field="amount" format="{0:c2}" />
+        <Column
+          title="Delete"
+          cell={props => {
+            const { dataItem } = props;
+            return (
+              <td>
+                <Button
+                  primary={true}
+                  onClick={e => {
+                    e.preventDefault();
+                    deleteTransaction(dataItem);
+                  }}
+                >
+                  Delete
+                </Button>
+              </td>
+            );
+          }}
+        ></Column>
       </Grid>
       <AddTransaction defaultCategory={category} budgetID={budgetID} />
+      <h1>Status</h1>
+      <ul>
+        <li>Total Budgeted: {formatMoney(category.amount)}</li>
+        <li>Total Spent: {formatMoney(totalSpent)}</li>
+      </ul>
     </div>
   );
 };
